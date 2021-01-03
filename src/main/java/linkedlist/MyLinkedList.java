accb061bfbd65efb503b4b826f029ce9b623ba31
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-01-04 01:42:25 +0530

    Added:Added linked list

--- src/main/java/linkedlist/MyLinkedList.java
+++ src/main/java/linkedlist/MyLinkedList.java
@@ -1,4 +1,27 @@
 package linkedlist;
 
-public class MyLinkedList {
+public class MyLinkedList<K extends Comparable<K>> {
+    public INode<K> head;
+    public INode<K> tail;
+    public MyLinkedList()
+    {
+        this.head=head;
+        this.tail=tail;
+    }
+    public void add(INode<K> myNode)
+    {
+        if (this.tail==null)
+        {
+            this.tail=myNode;
+        }
+        if (this.head==null)
+        {
+            this.head=myNode;
+        }
+        else {
+            INode tempNode=this.head;
+            this.head=myNode;
+            this.head.setNext(tempNode);
+        }
+    }
 }
